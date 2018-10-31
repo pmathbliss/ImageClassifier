82ce721b977d1b745cf43640c33e2b5db41a1462
Paul Bliss <pmathbliss@gmail.com>
2018-10-30 21:17:36 -0700

    Initial Commit Reads from a web cam source. Saves images to a Model/MODELNAME folder Adjust the camera, enter the score or classification and then hit save or enter.

---
+++ ImageClassifier/MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ImageClassifier
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.textBoxScore = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSaveModel = new System.Windows.Forms.Button();
+            this.comboBoxModel = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.comboBox1);
+            this.flowLayoutPanel1.Controls.Add(this.btnShow);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.textBoxScore);
+            this.flowLayoutPanel1.Controls.Add(this.btnSave);
+            this.flowLayoutPanel1.Controls.Add(this.videoSourcePlayer1);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.comboBoxModel);
+            this.flowLayoutPanel1.Controls.Add(this.btnSaveModel);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(653, 536);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video Camera";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(119, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(432, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(3, 93);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(638, 390);
+            this.videoSourcePlayer1.TabIndex = 2;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(557, 3);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(85, 39);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // textBoxScore
+            // 
+            this.textBoxScore.Location = new System.Drawing.Point(119, 48);
+            this.textBoxScore.Name = "textBoxScore";
+            this.textBoxScore.Size = new System.Drawing.Size(432, 26);
+            this.textBoxScore.TabIndex = 4;
+            this.textBoxScore.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxScore_KeyDown);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(557, 48);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(85, 39);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 5, 62, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Score";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 491);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 5, 62, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Model";
+            // 
+            // btnSaveModel
+            // 
+            this.btnSaveModel.Location = new System.Drawing.Point(558, 489);
+            this.btnSaveModel.Name = "btnSaveModel";
+            this.btnSaveModel.Size = new System.Drawing.Size(85, 39);
+            this.btnSaveModel.TabIndex = 8;
+            this.btnSaveModel.Text = "Save";
+            this.btnSaveModel.UseVisualStyleBackColor = true;
+            this.btnSaveModel.Click += new System.EventHandler(this.btnSaveModel_Click);
+            // 
+            // comboBoxModel
+            // 
+            this.comboBoxModel.FormattingEnabled = true;
+            this.comboBoxModel.Location = new System.Drawing.Point(120, 489);
+            this.comboBoxModel.Name = "comboBoxModel";
+            this.comboBoxModel.Size = new System.Drawing.Size(432, 28);
+            this.comboBoxModel.TabIndex = 10;
+            this.comboBoxModel.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxModel_KeyDown);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 536);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Image Classifier";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxScore;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxModel;
+        private System.Windows.Forms.Button btnSaveModel;
+    }
+}
+
---
+++ ImageClassifier/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+namespace ImageClassifier
+{
+    public partial class MainForm : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+        private PictureBox pictureBox;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice);
+            }
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "MonikerString";
+            comboBox1.SelectedIndex = 0;
+
+            videoSourcePlayer1.NewFrame += VideoSourcePlayer1_NewFrame;
+            pictureBox = new PictureBox();
+        }
+
+        private void VideoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            pictureBox.Image = image;
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var captureDevice = comboBox1.SelectedItem as FilterInfo;
+            videoSourcePlayer1.VideoSource = new VideoCaptureDevice(captureDevice.MonikerString);
+            videoSourcePlayer1.Start();
+            //videoSourcePlayer1.VideoSource = captureDevice.
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SavePicture();
+        }
+
+        private void textBoxScore_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                SavePicture();
+            }
+        }
+
+        /// <summary>
+        /// Save the current picture.
+        /// </summary>
+        private void SavePicture()
+        {
+            if(!videoSourcePlayer1.VideoSource.IsRunning)
+            {
+                MessageBox.Show("Select a video source and make sure it is running.");
+                return;
+            }
+
+
+            string score = textBoxScore.Text;
+
+            if (string.IsNullOrWhiteSpace(score))
+            {
+                MessageBox.Show("Enter a Value");
+                return;
+            }
+
+            //pause the videoSource
+            videoSourcePlayer1.VideoSource.Stop();
+
+            string modelName = comboBoxModel.Text;
+            string modelPath = Path.Combine("Models", modelName);
+
+            DateTime foo = DateTime.UtcNow;
+            long unixTime = ((DateTimeOffset)foo).ToUnixTimeSeconds();
+
+
+            string fileName = $"{unixTime}-{score}.jpg";
+
+            pictureBox.Image.Save(Path.Combine(modelPath, fileName));
+            videoSourcePlayer1.VideoSource.Start();
+
+        }
+
+        private void btnSaveModel_Click(object sender, EventArgs e)
+        {
+            CreateModelDirectory();
+        }
+
+        /// <summary>
+        /// Create the model directory and add the current selected model.
+        /// </summary>
+        private void CreateModelDirectory()
+        {
+            if (!Directory.Exists("Models"))
+            {
+                Directory.CreateDirectory("Models");
+            }
+
+            string modelName = comboBoxModel.Text;
+
+            if(string.IsNullOrWhiteSpace(modelName))
+            {
+                MessageBox.Show("Enter a model name");
+                return;
+            }
+
+            if (!Directory.Exists(Path.Combine("Models", modelName)))
+            {
+                Directory.CreateDirectory(Path.Combine("Models", modelName));
+            }
+
+            if(!comboBoxModel.Items.Contains(modelName))
+            {
+                comboBoxModel.Items.Add(modelName);
+            }
+        }
+
+        private void comboBoxModel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                CreateModelDirectory();
+            }
+        }
+    }
+}
